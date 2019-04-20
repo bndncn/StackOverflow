a61c4a23677cf3a6c043501a15ec27ed90c7bb25
Brandon Chen <brandon.chen@stonybrook.edu>
2019-04-20 17:10:23 -0400

    Added media GET

--- routes/media.js
+++ routes/media.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const Media = require('../models/media');
 const utils = require('../utils/service-utils');
 
 const media = express.Router();
 
 // Endpoint: /media/{id}
-media.get('/:id', function (req, res) {
+media.get('/:id', async function (req, res) {
     console.log('Getting media file: ' + req.params.id);
+
+    const id = req.params.id;
+
+    const mediaResult = await Media.findById({ id }).exec();
+
+    if (!mediaResult) {
+        console.log('Media id: ' + id + ' does not exist.');
+        return res.status(404).json(utils.errorJSON('Media id: ' + id + ' does not exist.'));
+    }
+    else {
+        res.contentType(mediaResult.mimetype);
+        return res.send(mediaResult.content);
+    }
 });
 
 module.exports = media;
