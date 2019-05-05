a3cfc019c81ee13f574ad9bd4930dbb76dd046fd
Brandon Chen <brandon.chen@stonybrook.edu>
2019-05-05 16:57:06 -0400

    Added css folder

--- routes/ui.js
+++ routes/ui.js
@@ -16,38 +16,38 @@ ui.get('/', function (req, res) {
 });
 
 ui.get('/controllers/form_controller.js', function (req, res) {
-    res.sendFile('/controllers/form_controller.js', {
-        root: '..'
+    res.sendFile('controllers/form_controller.js', {
+        root: __dirname
     });
 });
 
 ui.get('/node_modules/jquery/dist/jquery.min.js', function (req, res) {
-    res.sendFile('/node_modules/jquery/dist/jquery.min.js', {
-        root: '..'
+    res.sendFile('node_modules/jquery/dist/jquery.min.js', {
+        root: __dirname
     });
 });
 
 ui.get('/node_modules/bootstrap/dist/css/bootstrap.min.css', function (req, res) {
-    res.sendFile('/node_modules/bootstrap/dist/css/bootstrap.min.css', {
-        root: '..'
+    res.sendFile('node_modules/bootstrap/dist/css/bootstrap.min.css', {
+        root: __dirname
     });
 });
 
 ui.get('/node_modules/popper.js/dist/umd/popper.min.js', function (req, res) {
-    res.sendFile('/node_modules/popper.js/dist/umd/popper.min.js', {
-        root: '..'
+    res.sendFile('node_modules/popper.js/dist/umd/popper.min.js', {
+        root: __dirname
     });
 });
 
 ui.get('/node_modules/bootstrap/dist/js/bootstrap.min.js', function (req, res) {
-    res.sendFile('/node_modules/bootstrap/dist/js/bootstrap.min.js', {
-        root: '..'
+    res.sendFile('node_modules/bootstrap/dist/js/bootstrap.min.js', {
+        root: __dirname
     });
 });
 
 ui.get('/css/styles.css', function (req, res) {
-    res.sendFile('/css/styles.css', {
-        root: '..'
+    res.sendFile('css/styles.css', {
+        root: __dirname
     });
 });
 
